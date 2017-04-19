122d0ef9e035a13a0929ace18a6aee71fd2cb978
Ovidiu Mocan <lightway@192-168-0-106.rdsnet.ro>
2017-04-19 12:34:35 +0300

    add linting

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Arwen from './src/Arwen';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Arwen />
+
       </View>
     );
   }
---
+++ src/Arwen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Arwen extends Component {
+  render() {
+    return (
+			<Text>Open up main.js to start working on your app! 12 </Text>
+    );
+  }
+}
+
+export default Arwen;
